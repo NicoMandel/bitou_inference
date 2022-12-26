385477cc335289d036a1c57e18bda4bf353716a5
Nico Mandel <mandel.nicolas@gmail.com>
2022-12-26 13:07:32 +0100

    added reading in a file

--- scripts/basic_flask.py
+++ scripts/basic_flask.py
@@ -1,6 +1,12 @@
 # Following [this tutorial](https://pytorch.org/tutorials/intermediate/flask_rest_api_tutorial.html)
 
-from flask import Flask
+from flask import Flask, jsonify
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from PIL import Image
+import io
+import numpy as np
+from torchvision import models
 
 app = Flask(__name__)
 
@@ -8,3 +14,21 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/predict')
+def predict():
+    return jsonify({'class_id' : 'IMAGE_NET_XXX', 'class_name' : 'Cat'})
+
+
+def transform_image(image_bytes):
+    my_transforms = A.Compose([
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        ToTensorV2()
+    ])
+    image = np.asarray(Image.open(io.BytesIO(image_bytes)))
+    img_transformed = my_transforms(image=image)
+    return img_transformed['image'].unsqueeze(0)
+
+with open("../data/birdies_fuji_crops/crops/positive/DSCF7099_0.png", 'rb') as f:
+    image_bytes = f.read()
+    tensor = transform_image(image_bytes=image_bytes)
+    print(tensor)
