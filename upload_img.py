030e17200e4ad9d6d901f14ea8b4453fa695a6c7
Nico Mandel <mandel.nicolas@gmail.com>
2023-01-06 13:24:10 +0100

    removed additional function for rendering image

--- upload_img.py
+++ upload_img.py
@@ -38,10 +38,10 @@ def upload_image():
 		flash('Allowed image types are -> png, jpg, jpeg, gif')
 		return redirect(request.url)
 
-@app.route('/display/<filename>')
-def display_image(filename):
-	#print('display_image filename: ' + filename)
-	return redirect(url_for('static', filename=filename), code=301)
+# @app.route('/display/<filename>')
+# def display_image(filename):
+# 	#print('display_image filename: ' + filename)
+# 	return redirect(url_for('static', filename=filename), code=301)
 
 if __name__ == "__main__":
 	app.debug = True
