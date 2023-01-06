05130a6bb791c04d48675e6d934dcabca00c292a
Nico Mandel <mandel.nicolas@gmail.com>
2023-01-06 12:36:59 +0100

    resubmitted upload_img

--- upload_img.py
+++ upload_img.py
@@ -1,1 +1,49 @@
-app.py
+import os.path
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+fdir = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER = os.path.join(fdir, 'images')
+
+app = Flask(__name__, static_url_path=UPLOAD_FOLDER, static_folder=UPLOAD_FOLDER)
+app.secret_key = "secret_key"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 10 * 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+@app.route('/')
+def upload_form():
+	return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded and displayed below')
+		return render_template('upload.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename=filename), code=301)
+
+if __name__ == "__main__":
+	app.debug = True
+	port = 5000
+	app.run(host="0.0.0.0", debug=True, port=port)
